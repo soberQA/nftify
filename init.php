bd9d20a73472d5e202f180633fffa995f786da90
soberQA <127615504+soberQA@users.noreply.github.com>
2023-12-23 22:23:58 +0530

    Update init.php

--- init.php
+++ init.php
@@ -4,6 +4,9 @@
  * Plugin URI:		https://nftplugin.me
  * Description:	 	NFT plugin to turn your store into NFT marketplace
  * Version:		2.0.0
+  *License: 		GPL V2 or later
+  *License URI: 	LICENSE
+  *Tags: 		NFT, NFTify, Token-commerce
  */
 
 /**
